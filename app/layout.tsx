2cc2256f4d59e5cda05045bd9c0ec79934708af3
Paduma Induwara <induwarapaduma2002@gmail.com>
2026-01-21 11:01:39 +0530

    v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -388,6 +388,24 @@ export default function RootLayout({
         <meta name="geo.placename" content="Maharagama" />
         <meta name="geo.position" content="6.8412;79.9654" />
         <meta name="ICBM" content="6.8412, 79.9654" />
+       <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-0PQMQ2QCKB`} 
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-0PQMQ2QCKB', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#020010] text-white selection:bg-purple-500/30 selection:text-white overflow-x-hidden`}
